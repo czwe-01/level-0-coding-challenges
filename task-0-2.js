c75f2e523e54f40d7237bb236a037bdace6678a7
czwe-01 <tnkambule980115@gmail.com>
2021-09-09 23:56:11 +0200

    First commit

---
+++ task-0-2.js
@@ -0,0 +1,10 @@
+let x = 1 + 1 * 2;
+console.log(x);
+let y = (1 + 1) * 2;
+console.log(y);
+let z = 1 + 1 * 2;
+console.log(z);
+let a = 1 + (1 * 2) / 2;
+console.log(a);
+let b = (1 + 1 * 2) / 2;
+console.log(b);
