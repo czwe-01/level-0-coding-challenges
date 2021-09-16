2dd7c9ffd644fd9e050f41feb8bb3fc1dc4d0aa3
czwe-01 <tnkambule980115@gmail.com>
2021-09-16 13:15:16 +0200

    fixed function to not repeat vowels

--- task-0-9.js
+++ task-0-9.js
@@ -1,15 +1,18 @@
 function findVowels(string) {
   let vowelsToFind = /[aeiou]/gi;
   var length = string.length;
-  let vowels = "";
+  let vowels = new Array();
   let eachLetter = string.split("");
   for (let i = 0; i < length; i++) {
-    if (eachLetter[i].match(vowelsToFind)) {
-      vowels += eachLetter[i] + ", ";
+    if (
+      eachLetter[i].match(vowelsToFind) &&
+      vowels.indexOf(eachLetter[i].toLowerCase()) === -1
+    ) {
+      vowels.push(eachLetter[i].toLowerCase());
     }
   }
 
   console.log("Vowels: " + vowels);
 }
 
-findVowels("ThUlaSiIzwe");
+findVowels("Umuzi");
