157b3a1aae249d021dd0148a32b7970a2ce38be6
czwe-01 <tnkambule980115@gmail.com>
2021-11-03 14:15:29 +0200

    implemented the bot review

--- task-0-1.js
+++ task-0-1.js
@@ -1,11 +1,15 @@
-let x = 0;
-let y = 1;
+function valuesOfXandY() {
+  let x = 0;
+  let y = 1;
 
-console.log(x);
-console.log(y);
+  console.log(x);
+  console.log(y);
 
-x += 3;
-y += x;
+  x += 3;
+  y += x;
 
-console.log(x);
-console.log(y);
+  console.log(x);
+  console.log(y);
+}
+
+valuesOfXandY();
